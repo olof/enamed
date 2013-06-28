9f250851cd06bb9e3a62702ee78e9ca8b27fab25
Olof Johansson <olof@ethup.se>
2013-06-28 14:41:18 +0200

    Add named defines for DNS magic values
    Adds defines for class and rr values for those specified in RFC 1035.

--- dnsrecord.hrl
+++ dnsrecord.hrl
@@ -15,3 +15,25 @@
 	name, type, class, ttl, length, data
 }).
 
+% Classes, RFC 1035
+-define(DNS_CLASS_IN, 1).
+-define(DNS_CLASS_CS, 2).
+-define(DNS_CLASS_CH, 3).
+-define(DNS_CLASS_HS, 4).
+
+% RR Types, RFC 1035
+-define(DNS_RR_A, 1).
+-define(DNS_RR_NS, 2).
+-define(DNS_RR_MD, 3).
+-define(DNS_RR_MF, 4).
+-define(DNS_RR_CNAME, 5).
+-define(DNS_RR_SOA, 6).
+-define(DNS_RR_MB, 7).
+-define(DNS_RR_MG, 8).
+-define(DNS_RR_MR, 9).
+-define(DNS_RR_NULL, 10).
+-define(DNS_RR_WKS, 11).
+-define(DNS_RR_PTR, 12).
+-define(DNS_RR_HINFO, 13).
+-define(DNS_RR_MX, 15).
+-define(DNS_RR_TXT, 16).
